576f4dca05949cc2779e8f21f15f69b6f85bb445
Ehsan <ehsan@Ehsans-MacBook-Pro.local>
2019-09-11 00:18:59 -0700

    modified var and added checking print statements

--- scripts/python_codes/get_MISR_ML1B2T.py
+++ scripts/python_codes/get_MISR_ML1B2T.py
@@ -13,9 +13,9 @@ password = 'emosadegh@nevada.unr.edu'
 #------ setting for directory path 
 
 root_dir_local = '/Volumes/MISR_REPO/'
-#local_donwload_dir = '/Nolin/2001/Ml1b2e/August/'
+local_download_dir = '/Nolin/2001/Ml1b2e/August/'
 ftp_dir = '/PullDir/'
-#local_dir = root_dir_local+local_download_dir # local dir- ldir directory? DL dir???
+local_dir = root_dir_local+local_download_dir # local dir- check if it exists locally.
 
 # for MI1B2E Jul2016
 """
@@ -76,12 +76,11 @@ orders = ['0624864632',
 # for ML1BTE Aug2001
 order_ID_list = [
 	
-		'062816110196687',
-	  '062816109811111' ]
+		'062816110196687' ]  # '062816109811111'
 
 for order_ID in order_ID_list:
 
-	print(f'-> doing for order: {order_ID}')
+	print(f'-> processing order: {order_ID}')
    #local_dir = root_dir_local+local_download_dir # local dir- ldir directory? DL dir???
 
    #remote_order_dir = '/PullDir/' + order_ID + '/' # is it local or on the server? what is rdir directory?
@@ -95,53 +94,58 @@ for order_ID in order_ID_list:
 	#os.chdir(remote_order_dir)
 	print(f'-> we are at dir: { my_ftp.pwd() } ')
 
+	files_list = []  # for each loop inside a loop
 
+	my_ftp.dir(files_list.append)  # Produces a directory listing; does it work anymore???
 
+	print(f'-> size of list: { len(files_list) }')
 
-#    files_list = []
+	for file_to_download in files_list :
+		print(f'-> doing QA check...')
+		if (file_to_download.endswith('.hdf')):
 
-#    my_ftp.dir(files_list.append)  # Produces a directory listing; does it work anymore???
+			index_of_path = file_to_download.index('_P')
 
-#    for file_to_download in files_list:
+			path = int( file_to_download[ index_of_path + 2 : index_of_path + 5 ] )
+			print(f'-> path no. is= {path}')
+			index_of_orbit = file_to_download.index('_O')
 
-#       if (file_to_download.endswith('.hdf')):
+			orbit = int( file_to_download[ index_of_orbit + 2 : index_of_orbit + 8 ] )
+			print(f'-> orbit no. is= {orbit}')
+  		### what QA should we use here? based on file name?
 
-#          index_of_path = file_to_download.index('_P')
+	 #if (True):
+         #if (path > 45) and (path < 100): #Arctic SeaIce
+         #if (path > 222) or (path < 51):
+         #if (path < 80) and (path > 72):
+         #if (path < 66) and (path > 59):
 
-#          path = int( file_to_download[ index_of_path + 2 : index_of_path + 5 ] )
+			# if (file_to_download.find('CLOUD') < 0) :  # if files have CLOUD (?) then do not download?
+			# 	print(f'-> the file includes "CLOUD" keyword, so we skip it')
+			# 	continue 
+             #if (file_to_download.find('ELLIPSOID') < 0): continue
+             #if (file_to_download.find('TERRAIN') < 0): continue
+             #if (file_to_download.find('.f') > 0): continue
+             #if (file_to_download.find('GMP') < 0): continue
 
-#          index_of_orbit = file_to_download.index('_O')
-
-#          orbit = int( file_to_download[ index_of_orbit + 2 : index_of_orbit + 8 ] )
-
-# 	 if (True):
-#          #if (path > 45) and (path < 100): #Arctic SeaIce
-#          #if (path > 222) or (path < 51):
-#          #if (path < 80) and (path > 72):
-#          #if (path < 66) and (path > 59):
-
-#              if (file_to_download.find('CLOUD') < 0): continue  # if files have CLOUD (?) then do not download?
-#              #if (file_to_download.find('ELLIPSOID') < 0): continue
-#              #if (file_to_download.find('TERRAIN') < 0): continue
-#              #if (file_to_download.find('.f') > 0): continue
-#              #if (file_to_download.find('GMP') < 0): continue
-
-# 	     index_of_MISR = file_to_download.index('MISR_')
+			index_of_MISR = file_to_download.index('MISR_')
 	     
-#              remote_file = file_to_download[ index_of_MISR : ]  # ???
+			remote_file = file_to_download[ index_of_MISR : ]  # ???
 
-#              if (not os.path.exists(local_dir + remote_file)):
+			if ( not os.path.exists( local_dir + remote_file ) ) :
 
-#                 print ( f'-> this remote file does not exist on FTP server: {remote_file}')
+				print ( f'-> we do not have this file yet: {remote_file}')
 
-#                 downloading_file = open(local_dir + remote_file, 'wb')  # w= write to file, b= in binary mode
-#                 # ???????
-#                 try:
-#                         my_ftp.retrbinary('RETR %s' % remote_file, downloading_file.write)  # Retrieve a file in binary transfer mode
-#                         downloading_file.close()
+	      # downloading_file = open(local_dir + remote_file, 'wb')  # w= write to file, b= in binary mode
 
-#                 except ftplib.error_temp:
-#                         print ('FTP ERROR: checksum failure on file "%s/%s"' % (remote_order_dir, remote_file))
+	      # try :
+	      	
+	      # 	my_ftp.retrbinary('RETR %s' % remote_file, downloading_file.write)  # Retrieve a file in binary transfer mode
+	      #   downloading_file.close()
 
-# my_ftp.close()
+	      # except ftplib.error_temp :
+
+	      # 	print ('FTP ERROR: checksum failure on file "%s/%s"' % (remote_order_dir, remote_file))
+
+my_ftp.close()
 
