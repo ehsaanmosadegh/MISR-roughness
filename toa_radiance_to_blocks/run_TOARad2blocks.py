a812d539af4fa5ab7ab15aa81e703f37dea5ae41
ehsaanmosadegh <emosadegh@nevada.unr.edu>
2019-12-25 19:36:48 -0800

    renamed and added some var

--- toa_radiance_to_blocks/run_TOARad2blocks.py
+++ toa_radiance_to_blocks/run_TOARad2blocks.py
@@ -36,7 +36,7 @@ output_dir_name = 'toa_radiance_July_2016/test1'	# path to toa dir
 # other settings - do not change
 
 exe_name = 'TOARad2blocks'
-block_range = [1,43] # should match with block range in downloading step
+block_range = [1,43] # [start, stop]; should match with block range in downloading step
 band_list = ['Red']
 band_num = 3
 # minnert = is set inside the program
@@ -48,49 +48,51 @@ def main():
 	'''
 	passes a pair of argumenst to cmd to run TOA.c program
 	'''
-	list_of_misr_files_fullpath, output_dir, band_no, minnaert = local_file_setting(root_dir, MISR_download_dir_name, root_dir, output_dir_name, band_list)
+	list_of_misr_files_fullpath, output_dir, band_no, minnaert = dir_n_file_setting(root_dir, MISR_download_dir_name, root_dir, output_dir_name, band_list)
 	
-	for each_hdf_file in list_of_misr_files_fullpath:
+	for each_ellipsoid_file in list_of_misr_files_fullpath:
 
-		path, orbit, camera = parse_file_names(each_hdf_file)
+		path, orbit, camera = parse_file_names(each_ellipsoid_file)
 
-		for each_block in block_range: # why loop over 42 blocks in one hdf file
+		for each_block in range(block_range[0], block_range[1], 1): # why loop over 42 blocks in one hdf file
 
-			toa_file_fullpath = define_toa_files(path, orbit, each_block, camera, output_dir)
+			toa_file_fullpath = define_toa_rad_files(path, orbit, each_block, camera, output_dir)
+
 			# now run TOA from linux to process Ellipsoid data
-			#run_from_cmd(exe_name, each_hdf_file, each_block, band_no, minnaert, toa_file_fullpath)
+			#run_from_cmd(exe_name, each_ellipsoid_file, each_block, band_no, minnaert, toa_file_fullpath)
 
 	return 0
 
 ###############################################################################
 
-def define_toa_files(path, orbit, each_block, camera, output_dir):
+def define_toa_rad_files(path, orbit, each_block, camera, output_dir):
 
 	if (each_block <= 9):
-		print('-> block was: %s' %each_block)
+		#print('-> block was: %s' %each_block)
 		each_block = str(each_block).rjust(2, '0')
 		print('-> rjust performed, block is: %s' %each_block)
-	else:
-		pass
+	# else:
+	# 	pass
+
 	# toa output file names to CMD command --> to do: make function for this section
 	toa_file_pattern = ('toa_rad_%s_%s_B%s_%s.dat' %(path, orbit, each_block, camera)) # will be written by TOA
 	toa_file_fullpath = os.path.join(output_dir, toa_file_pattern)
-	print('-> %s program writes toa radiance data to file= %s' % (exe_name, toa_file_fullpath))
+	print('-> %s writes toa radiance data to file= %s' % (exe_name, toa_file_fullpath))
 
 	return toa_file_fullpath
 
 ###############################################################################
 
-def run_from_cmd(exe_name, each_hdf_file, each_block, band_no, minnaert, toa_file_fullpath):
+def run_from_cmd(exe_name, each_ellipsoid_file, each_block, band_no, minnaert, toa_file_fullpath):
 
 	# this is removed from the original C code, so we do not need image anymore
 	# toa_image_file = '%stoa_%s_%s_b%s_%s.png' % (output_dir, path, orbit, each_block, camera)
 	# print(toa_image_file)
 
 	# run the C-cmd program
-	#cmd = 'TOA3 "%s" %s %s %s \"%s\" \"%s\"' %(each_hdf_file, each_block, band_no, minnaert, toa_file_fullpath, toa_image_file) # old version
+	#cmd = 'TOA3 "%s" %s %s %s \"%s\" \"%s\"' %(each_ellipsoid_file, each_block, band_no, minnaert, toa_file_fullpath, toa_image_file) # old version
 	print('-> program-name	radiance-file-name	block 	band 	minnaert	path-to-toa-dataFile')
-	cmd = (' "%s" "%s" %s %s %s \"%s\"' %(exe_name, each_hdf_file, each_block, band_no, minnaert, toa_file_fullpath))  # TOA writes data into toa_file_fullpath
+	cmd = (' "%s" "%s" %s %s %s \"%s\"' %(exe_name, each_ellipsoid_file, each_block, band_no, minnaert, toa_file_fullpath))  # TOA writes data into toa_file_fullpath
 	print('-> runScript to %s= %s' %(exe_name, cmd))	# run the cmd command.
 
 	# if os.system(cmd) != 0 :
@@ -104,22 +106,29 @@ def run_from_cmd(exe_name, each_hdf_file, each_block, band_no, minnaert, toa_fil
 
 ###############################################################################
 
-def local_file_setting(input_dir_path, input_dir_name, root_dir, output_dir_name, band_list):
+def dir_n_file_setting(input_dir_path, input_dir_name, root_dir, output_dir_name, band_list):
 	'''
 	reads dir paths and check if they exist, lists all ellipsoid files and returns a list of files w/ fullpath
 	'''
 
 	download_dir = os.path.join(root_dir, MISR_download_dir_name)
 	output_dir = os.path.join(root_dir, output_dir_name)
-	print('-> output dir: %s' %output_dir)
+	print('-> input dir : %s' % (download_dir))
+	print('-> output dir: %s' % (output_dir))
 	# check if directories exist
 	if not (os.path.isdir(download_dir)):
-		print('-> download directory NOT exist!')
+		print('-> download directory NOT exist! check the path')
 		raise SystemExit()
 
 	if not (os.path.isdir(output_dir)):
 		print('-> output directory NOT exist!')
-		raise SystemExit()
+		print('-> we try to create the output dir')
+		os.mkdir(output_dir)
+		if not (os.path.isdir(output_dir)):
+			print('-> we could not create outpur dir. You should create it manually.')
+			raise SystemExit()
+		else:
+			print('-> output dir created successfully!')
 
 	# checking some constants
 	for band in band_list:
@@ -137,31 +146,31 @@ def local_file_setting(input_dir_path, input_dir_name, root_dir, output_dir_name
 
 	# get a list of available/downloaded Ellipsoid files, the list will be list of file_fullpath-s 
 	list_of_misr_files_fullpath = glob.glob(os.path.join(download_dir, misr_file_patern))
-	print('-> no. of hdf files found= %s' %len(list_of_misr_files_fullpath))
+	print('-> no. of "GRP_ELLIPSOID" files found= %s' %len(list_of_misr_files_fullpath))
 
 	return list_of_misr_files_fullpath, output_dir, band_no, minnaert
 
 ###############################################################################
 
-def parse_file_names(each_hdf_file):
+def parse_file_names(each_ellipsoid_file):
 
-	print('-> hdf file is= %s' %each_hdf_file)
+	print('-> Ellipsoid rad file= %s' %each_ellipsoid_file)
 
 	# get the index of path and the path number
-	path_index = each_hdf_file.index('_P')
-	path = each_hdf_file[ path_index+1 : path_index+5 ]
+	path_index = each_ellipsoid_file.index('_P')
+	path = each_ellipsoid_file[ path_index+1 : path_index+5 ]
 
 
 	# get the index of orbit and the orbit number
-	orbit_index = each_hdf_file.index('_O')
-	orbit = each_hdf_file[ orbit_index+1 : orbit_index+8]
+	orbit_index = each_ellipsoid_file.index('_O')
+	orbit = each_ellipsoid_file[ orbit_index+1 : orbit_index+8]
 
 	# find the camera in the file name
-	if each_hdf_file.find('_CF') != -1 :
+	if each_ellipsoid_file.find('_CF') != -1 :
 		camera = 'cf'
-	elif each_hdf_file.find('_CA') != -1 :
+	elif each_ellipsoid_file.find('_CA') != -1 :
 		camera = 'ca'
-	elif each_hdf_file.find('_AN') != -1 :
+	elif each_ellipsoid_file.find('_AN') != -1 :
 		camera = 'an'
 	else:
 		print('-> ERROR: camera NOT found!')
