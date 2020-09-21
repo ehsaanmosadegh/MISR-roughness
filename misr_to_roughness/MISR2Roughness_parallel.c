00da7730be512e3a8dd1d992e4ab95d97704ca8a
ehsan mosadegh <ehsan.mosadegh@gmail.com>
2020-09-20 22:43:07 -0700

    updated paths for misr parallel

--- misr_to_roughness/MISR2Roughness_parallel.c
+++ misr_to_roughness/MISR2Roughness_parallel.c
@@ -104,16 +104,16 @@ int main() {
 	// char masked_toa_an_dir[256] = "/home/mare/Nolin/data_2000_2016/2016/Surface3_LandMasked/Jul/An/test_ehsan"; // output of LandMask.c - use masked_surf files instead
 	
 	// path on my Mac
-	char masked_toa_an_dir[256] = "/Volumes/Ehsanm_DRI/research/MISR/masked_toa_files/masked_toa_refl_ellipsoid_apr2013_day1to16_p1to233_b1to40/An" ; // path to An dir files, we use An camera to define file labels for Ca Cf cameras
+	// char masked_toa_an_dir[256] = "/Volumes/Ehsanm_DRI/research/MISR/masked_toa_files/masked_toa_refl_ellipsoid_apr2013_day1to16_p1to233_b1to40/An" ; // path to An dir files, we use An camera to define file labels for Ca Cf cameras
 	
 	// path on Pronghorn cluster
-	// char masked_toa_an_dir[256] = "/data/gpfs/assoc/misr_roughness/masked_toa_refl_ellip_apr2013_d1to16_p1to233_b1to40/An" ;
+	char masked_toa_an_dir[256] = "/data/gpfs/assoc/misr_roughness/masked_toa_refl_ellip_apr2013_d1to16_p1to233_b1to40/An" ;
 
 	// old path on Anne's Linux machine
    // char atmmodel_csvfile[256] = "/home/mare/Projects/MISR/Julienne/IceBridge2016/SeaIce_Jul2016_atmmodel_csvfile2_r025.csv"; // ATM csv file; source from where/
 	
-	char atmmodel_csvfile[256] = "/Volumes/Ehsanm_DRI/research/MISR/atmmodel_dir/atmmodel_2013/atmmodel_2013_aug1_16_b1_40_newASCM.csv" ; // ATM csv file; source from where/
-	// char atmmodel_csvfile[256] = "/data/gpfs/assoc/misr_roughness/atmmodels/atmmodel_2013_aug1_16_b1_40_newASCM.csv" ;
+	// char atmmodel_csvfile[256] = "/Volumes/Ehsanm_DRI/research/MISR/atmmodel_dir/atmmodel_2013/atmmodel_2013_aug1_16_b1_40_newASCM.csv" ; // ATM csv file; source from where/
+	char atmmodel_csvfile[256] = "/data/gpfs/assoc/misr_roughness/atmmodels/atmmodel_2013_aug1_16_b1_40_newASCM.csv" ;
 
 
 	// we don't use this file anymore, we decided to use all blocks, meaning no correction/reversing Cf and Ca cameras.
@@ -121,7 +121,8 @@ int main() {
 
 	
 	// outputs 
-	char predicted_roughness_dir[256] = "/Volumes/Ehsanm_DRI/research/MISR/roughness_files/multithreaded_atmmodel_newASCM_test_Path40" ; // MISR roughness; rms file; no "/" at the end
+	// char predicted_roughness_dir[256] = "/Volumes/Ehsanm_DRI/research/MISR/roughness_files/multithreaded_atmmodel_newASCM_test_Path40" ; // MISR roughness; rms file; no "/" at the end
+	char predicted_roughness_dir[256] = "/data/gpfs/assoc/misr_roughness/roughness_2013_apr1to16_p1to233_b1to40" ; // MISR roughness; rms file; no "/" at the end
 	
 	// other variables
 	char command[256];
