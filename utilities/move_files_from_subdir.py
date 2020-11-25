3af691543b9328d740792ee04721d03023b4a80d
ehsan mosadegh <ehsan.mosadegh@gmail.com>
2020-11-24 23:00:15 -0800

    added new oath

--- utilities/move_files_from_subdir.py
+++ utilities/move_files_from_subdir.py
@@ -8,7 +8,7 @@
 import os, glob, shutil
 
 src_dir_fullpath = '/data/gpfs/assoc/misr_roughness/2016/april_2016/remainder_xml_files'
-target_dir_fullpath = '/data/gpfs/assoc/misr_roughness/2016/april_2016/remainder_xml_files'
+target_dir_fullpath = src_dir_fullpath
 
 
 #~ define file pattern
