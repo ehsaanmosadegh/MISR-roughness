8f772ef4300b1e569c65c7638fec4b2dcd76962f
ehsan mosadegh <ehsan.mosadegh@gmail.com>
2020-11-24 23:51:10 -0800

    new path

--- utilities/move_files_from_subdir.py
+++ utilities/move_files_from_subdir.py
@@ -7,14 +7,14 @@
 
 import os, glob, shutil
 
-src_dir_fullpath = '/data/gpfs/assoc/misr_roughness/2016/april_2016/remainder_xml_files'
+src_dir_fullpath = '/data/gpfs/assoc/misr_roughness/2016/july_2016/ATM_july_13_21/
 target_dir_fullpath = src_dir_fullpath
 
 
 #~ define file pattern
 # file_pattern = 'MISR_AM1_GRP_ELLIPSOID_GM_P*'+'.hdf'
-# file_pattern = 'ILATM2*.csv'
-file_pattern = 'MISR_AM1_TC_CLASSIFIERS*.hdf'
+file_pattern = 'ILATM2*.csv'
+# file_pattern = 'MISR_AM1_TC_CLASSIFIERS*.hdf'
 
 
 print('source dir: %s' % src_dir_fullpath)
