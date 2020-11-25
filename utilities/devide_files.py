98ea83e1b3c3edd682ed541985f3b09e1996b153
ehsan mosadegh <ehsan.mosadegh@gmail.com>
2020-11-24 16:45:30 -0800

    added new script

---
+++ utilities/devide_files.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python
+
+import glob, shutil
+
+#~ to devide files from whole to n-subdir to process parallel
+src_dir = '/data/gpfs/assoc/misr_roughness/2016/july_2016'
+dest_dir = src_dir
+
+file_pattern = 'MISR_AM1_GRP_ELLIPSOID_GM_P*';
+dest_subdir_list = ['part_1', 'part_2', 'part_3', 'part_4'];
+
+src_files_fp_list = glob.glob(os.path.join(src_dir, file_pattern));
+print('num. of files found: %s' % len(src_files_fp_list));
+
+
+edgeCntr_list = range(0,len(src_files_fp_list),1000)
+edgeCntr_list.append(len(src_files_fp_list))
+
+for edgeCntr in range(len(edgeCntr_list)-1):
+	#~ select a subdir 
+	dest_subdir_fp = os.path.join(dest_dir, dest_subdir_list[edgeCntr])
+
+	for fileCntr in range(edgeCntr_list[edgeCntr], edgeCntr_list[edgeCntr+1]):
+		#~ check is file exists in destination
+		if (os.path.isfile(os.path.join(dest_subdir_fp, src_files_fp_list[fileCntr].split('/')[-1]))==True):
+				continue
+			#~ move files from src dir to target dir
+			shutil.move(src_files_fp_list[fileCntr], dest_subdir_fp)
+			#~ check if file was moved successfully
+			if (os.path.isfile(os.path.join(dest_subdir_fp, src_files_fp_list[fileCntr].split('/')[-1]))):
+				print('file moved successfully!')
+			else:
+				print('warning on moving file')
+
