4a7899f17b55b23eecf0b22e5244ad32f46971b8
ehsan mosadegh <ehsan.mosadegh@gmail.com>
2020-12-04 18:54:00 -0800

    rm dir path files

--- exe_dir/dir_path_setting_on_Mac.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# export PATH=$(pwd):$PATH
-export PATH=/Users/ehsanmos/Documents/MISR/MISR-roughness/exe_dir:$PATH
--- exe_dir/dir_path_setting_on_PH.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-# export PATH=$(pwd):$PATH
-export PATH=/data/gpfs/home/emosadegh/MISR-roughness/exe_dir:$PATH
